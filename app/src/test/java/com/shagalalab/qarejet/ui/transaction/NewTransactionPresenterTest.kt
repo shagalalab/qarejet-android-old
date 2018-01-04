b9d87004eb9cc7f2c4433124544eb1ff14a2c61f
Atabek <atabekmur@gmail.com>
2018-01-04 22:03:49 +1100

    Add unit tests for new transaction presenters addNewTransaction method

--- app/src/test/java/com/shagalalab/qarejet/ui/transaction/NewTransactionPresenterTest.kt
+++ app/src/test/java/com/shagalalab/qarejet/ui/transaction/NewTransactionPresenterTest.kt
@@ -1,9 +1,6 @@
 package com.shagalalab.qarejet.ui.transaction
 
-import com.nhaarman.mockito_kotlin.times
-import com.nhaarman.mockito_kotlin.verify
-import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
-import com.nhaarman.mockito_kotlin.whenever
+import com.nhaarman.mockito_kotlin.*
 import com.shagalalab.qarejet.TestSchedulers
 import com.shagalalab.qarejet.domain.interactor.account.GetAllAccountsUseCase
 import com.shagalalab.qarejet.domain.interactor.category.GetAllCategoriesUseCase
@@ -12,12 +9,13 @@ import com.shagalalab.qarejet.domain.model.Account
 import com.shagalalab.qarejet.domain.model.Category
 import com.shagalalab.qarejet.domain.model.Transaction
 import com.shagalalab.qarejet.domain.repository.AccountRepository
-import com.shagalalab.qarejet.domain.repository.TransactionRepository
 import com.shagalalab.qarejet.util.Constants
+import io.reactivex.Completable
 import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.anyString
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
@@ -28,17 +26,13 @@ class NewTransactionPresenterTest {
     private val accounts = listOf(Account(1, "one"), Account(2, "two"))
     private val categories = listOf(Category(1, "one", 1), Category(2, "two", 2))
     private val transaction = Transaction(1, 1, Date(), 1, 1, 1.0, "")
+    private val schedulersProvider = TestSchedulers()
 
     @Mock private lateinit var view: NewTransactionView
     @Mock private lateinit var getAllCategoriesUseCase: GetAllCategoriesUseCase
-
-    @Mock private lateinit var transactionRepository: TransactionRepository
     @Mock private lateinit var addNewTransactionsUseCase: AddNewTransactionUseCase
-
     @Mock private lateinit var accountRepository: AccountRepository
     @InjectMocks private lateinit var getAllAccountsUseCase: GetAllAccountsUseCase
-
-    private val schedulersProvider = TestSchedulers()
 
     private lateinit var presenter: NewTransactionPresenter
 
@@ -101,6 +95,22 @@ class NewTransactionPresenterTest {
     fun shouldShowErrorIfNewTransactionAmountIsZero() {
         presenter.addNewTransaction(0.0, transaction.accountId, transaction.categoryId, transaction.memo, transaction.date)
         verify(view, times(1)).showError(anyInt())
+        verifyNoMoreInteractions(view)
+    }
+
+    @Test
+    fun shouldAddNewTransaction() {
+        whenever(addNewTransactionsUseCase.execute(any())).thenReturn(Completable.complete())
+        presenter.addNewTransaction(transaction.amount, transaction.accountId, transaction.categoryId, transaction.memo, transaction.date)
+        verify(view, times(1)).finishActivity()
+        verifyNoMoreInteractions(view)
+    }
+
+    @Test
+    fun shouldShowErrorIfAddNewTransactionIsFailed() {
+        whenever(addNewTransactionsUseCase.execute(any())).thenReturn(Completable.error(Throwable()))
+        presenter.addNewTransaction(transaction.amount, transaction.accountId, transaction.categoryId, transaction.memo, transaction.date)
+        verify(view, times(1)).showError(anyString())
         verifyNoMoreInteractions(view)
     }
 }
