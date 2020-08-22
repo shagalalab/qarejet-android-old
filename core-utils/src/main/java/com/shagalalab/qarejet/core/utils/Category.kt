e58efe5ac77ca0a1b2790b676153b75b25e63355
Atabek <atabekmur@gmail.com>
2020-08-22 22:38:09 +1000

    Fix crash

--- core-utils/src/main/java/com/shagalalab/qarejet/core/utils/Category.kt
+++ core-utils/src/main/java/com/shagalalab/qarejet/core/utils/Category.kt
@@ -1,11 +1,13 @@
 package com.shagalalab.qarejet.core.utils
 
+import java.io.Serializable
+
 data class Category(
     val id: Long,
     val title: String,
     val icon: String,
     val color: String,
     val type: Int
-) {
+) : Serializable {
     override fun toString() = title
 }
